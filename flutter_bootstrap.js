81c2114275f541f5c7a36c265d0d5028214f98fe
Ahmed Yasser Taha <ayt614614@gmail.com>
2024-12-30 00:02:43 +0200

    finsh webs

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "954731294"
+    serviceWorkerVersion: "637446706"
   }
 });
